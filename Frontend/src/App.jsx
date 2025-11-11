cad0145c5dd92b522ebda41c973430e1d8e3d6fb
Sourav kumar jha <sunny@Souravs-MacBook-Air.local>
2025-11-11 01:50:35 -0500

    Backend completed and frontend started Routing for Signup and Sign in page done

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Signup from './Pages/Signup'
+import Signin from './Pages/Signin'
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/signin' element={<Signin />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ Frontend/src/Pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import bg from "../assets/authBg.png"
+const Signup = () => {
+  return (
+    <div className='w-full h-[100vh] bg-cover flex items-center justify-center' style={{backgroundImage:`url(${bg})`}}>
+        <form className=''></form>
+    </div>
+  )
+}
+
+export default Signup
