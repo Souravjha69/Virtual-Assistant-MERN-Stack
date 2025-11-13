a421dc9aaf63094e8bf686101da2343704ebcdc7
Sourav kumar jha <sunny@Souravs-MacBook-Air.local>
2025-11-13 01:35:11 -0500

    Sign up page is created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Signup from './Pages/Signup'
-import Signin from './Pages/Signin'
-const App = () => {
+import { Routes, Route } from "react-router-dom";
+import Signup from "./Pages/Signup";
+import Signin from "./Pages/Signin";
+
+export default function App() {
   return (
     <Routes>
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/signin' element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
     </Routes>
-  )
+  );
 }
-
-export default App
